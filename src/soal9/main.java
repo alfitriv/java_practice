9039b85952d4c5125e99d24cbe11a415a1b61dde
Mila <vania.alfitri@go-jek.com>
2017-05-15 18:00:51 +0700

    fixed soal 9

--- src/soal9/main.java
+++ src/soal9/main.java
@@ -7,20 +7,29 @@ public class main {
     /*Buat object Animal, dgn default constructor isAlive (boolean), name (string),
     age (int), lalu print masing2 properties object tsb (3 points)
      */
+    myObject animal = new myObject();
 
-    class myObject{
+}
+
+    class myObject {
         boolean isAlive;//instance variables
         String name;
         int age;
 
-        public myObject()
-        {
+        public myObject() {
             isAlive = true;
             name = "Lion";
             age = 12;
         }
 
+        public void print() {
+            System.out.println(name + isAlive + age);
+        }
     }
-    myObject animal = new myObject();
 
-}
+
+
+
+
+
+

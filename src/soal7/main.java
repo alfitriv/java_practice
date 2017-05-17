e915260b0f2cc27cdd41a6129570a94403ac996a
Mila <vania.alfitri@go-jek.com>
2017-05-17 17:23:37 +0700

    added refactoring for soal7 answers

--- src/soal7/main.java
+++ src/soal7/main.java
@@ -7,18 +7,22 @@ public class main {
     /*Buat lah beberapa variable int dgn value angka genap dan ganjil, finally, buat
     if else statement yg menentukan angka tersebut ganjil atau genap (bonus point: kalau klian pakai function) (2 point, 1 bonus point)
      */
-    public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5};
-
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 == 0) {
-                System.out.println("genap");
-            } else {
-                System.out.println("ganjil");
-            }
-
+    public static void isEven(int nums){
+        if (nums % 2 == 0) {
+            System.out.println("genap");
+        } else {
+            System.out.println("ganjil");
         }
+
+    }
+    public static void main(String[] args) {
+        /*int[] nums = {1, 2, 3, 4, 5};
+
+
+        for (int i = 0; i < nums.length; i++)*/
+
+        isEven(5);
+
 
     }
 }
